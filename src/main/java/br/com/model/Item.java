9ae2a6c74b6f559566fc831c72ab179078bb4389
Matheus Souza <matheusmargarido.contato@gmail.com>
2025-05-18 21:14:32 -0300

    feat: adicionar classe Item para controle de estoque

---
+++ src/main/java/br/com/model/Item.java
@@ -0,0 +1,35 @@
+package br.com.model;
+
+import java.util.Date;
+
+public class Item {
+    private int id;
+    private String nome;
+    private int quantidade;
+    private Date dataCadastro;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+}
