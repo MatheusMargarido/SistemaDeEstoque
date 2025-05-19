f8c89f0de1c36346bd7e3061f9051334dc936464
Matheus Souza <matheusmargarido.contato@gmail.com>
2025-05-18 21:47:59 -0300

    chore: utilizar variáveis de ambiente para conexão ao banco

--- src/main/java/br/com/factory/ConnectionFactory.java
+++ src/main/java/br/com/factory/ConnectionFactory.java
@@ -4,18 +4,21 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionFactory {
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "Cnv@x#2023!";
-    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/estoque?useSSL=false&serverTimezone=UTC";
-    
+    // Pegando credenciais do banco de dados a partir de variáveis de ambiente
+    private static final String USERNAME = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASS");
+    private static final String DATABASE_URL = System.getenv("DB_URL");
+
     public static Connection createConnectionToMySQL() throws Exception {
-        Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
-        return connection;
+        if (USERNAME == null || PASSWORD == null || DATABASE_URL == null) {
+            throw new IllegalStateException("Variáveis de ambiente do banco de dados não configuradas!");
+        }
+        return DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
     }
-    
+
     public static void main(String[] args) throws Exception {
         try (Connection con = createConnectionToMySQL()) {
             System.out.println("Conexão obtida com sucesso!");
         }
-    } 
+    }
 }
